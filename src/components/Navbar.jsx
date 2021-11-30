9b3ae9abca3e25d1380dc38ad0ee29759e64e63f
Kaung Khant Zaw <kaungkzdev@gmail.com>
2021-11-30 17:31:32 +0630

    fixed hot reloadig not working when classes changed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,30 @@
 import React, { useEffect, useState } from "react";
 import { Routes, Route, Outlet, Link } from "react-router-dom";
+// import { logo } from "../../public/images/logo.png";
 
 export default function Navbar() {
-  const [test, setTest] = useState([]);
-  useEffect(() => {
-    // console.log("rendered");
-    fetch("/data/data.json")
-      .then((data) => data.json())
-      .then((data) => setTest(data));
-  }, []);
+  // const [test, setTest] = useState([]);
+  // useEffect(() => {
+  //   fetch("/data/data.json")
+  //     .then((data) => data.json())
+  //     .then((data) => setTest(data));
+  // }, []);
 
   return (
     <div>
       <nav>
-        <ul>
-          {test.map((v) => {
+        <ul className="flex">
+          <li>
+            <Link to="/" className="text-primary-default">
+              <img src="/images/logo.png" alt="" />
+            </Link>
+          </li>
+          <li>
+            <button className="border border-primary-default text-primary-default p-2 m-5">
+              Download Resume
+            </button>
+          </li>
+          {/* {test.map((v) => {
             return (
               <li>
                 <Link to={v.url} className="text-primary-default">
@@ -22,7 +32,7 @@ export default function Navbar() {
                 </Link>
               </li>
             );
-          })}
+          })} */}
         </ul>
       </nav>
       <Outlet />
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,11 @@ const colors = require("tailwindcss/colors");
 
 module.exports = {
   mode: "jit",
-  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  purge: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html",
+    "./src/components/*.{js,jsx}",
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     fontFamily: {
